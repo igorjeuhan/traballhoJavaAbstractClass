4a4b2fca3fc7598937230551cdd3b18af2db1e2a
Igor Jeuhan <igorjeuhan10@gmail.com>
2019-11-21 13:38:02 -0200

    Classe produto

---
+++ src/classesabstratas/Produto.java
@@ -0,0 +1,48 @@
+package classesabstratas;
+
+public abstract class Produto {
+    
+    private String cod;
+    private String nome;
+    private String descricao;
+    private double preco;
+    
+    public Produto (String c, String n, String d, double p) {
+        this.setCod(c);
+        this.setNome(n);
+        this.setDesc(d);
+        this.setPreco(p);
+    }
+    
+    public void setCod(String c) {
+        this.cod = c;
+    }
+    
+    public String getCod(){
+        return this.cod;
+    }
+    
+    public void setNome(String n) {
+        this.nome = n;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public void setDesc(String d){
+        this.descricao = d;
+    }
+    
+    public String getDesc(){
+        return this.descricao;
+    }
+    
+    public void setPreco(double p) {
+        this.preco = p;
+    }
+    
+    public double getPreco() {
+        return this.preco;
+    }
+}
